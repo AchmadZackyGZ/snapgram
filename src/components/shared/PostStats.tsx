3f353624a6eca1c9cfe4e0b00d8fdd3d668ebd86
Achmad Zacky Ghoutsu Zamani <achmadzacky458@gmail.com>
2024-02-13 13:14:02 +0700

    first commit

---
+++ src/components/shared/PostStats.tsx
@@ -0,0 +1,105 @@
+import {
+  useDeleteSavedPost,
+  useGetCurrentUser,
+  useLikePost,
+  useSavePost,
+} from "@/lib/react-query/queriesAndMutation";
+import { checkIsLiked } from "@/lib/utils";
+import { Models } from "appwrite";
+import React, { useState, useEffect } from "react";
+import Loader from "./Loader";
+
+type PostStatsProps = {
+  post?: Models.Document;
+  userId: string;
+};
+
+const PostStats = ({ post, userId }: PostStatsProps) => {
+  const likesList = post?.likes.map((user: Models.Document) => user.$id);
+
+  const [likes, setLikes] = useState(likesList);
+  const [isSaved, setIsSaved] = useState(false);
+
+  const { mutate: likePost } = useLikePost();
+  const { mutate: savePost, isPending: isSavingPost } = useSavePost();
+  const { mutate: deleteSavedPost, isPending: isDeletingSaved } =
+    useDeleteSavedPost();
+
+  const { data: currentUser } = useGetCurrentUser();
+
+  const savedPostRecord = currentUser?.save.find(
+    (record: Models.Document) => record.post.$id === post?.$id
+  );
+
+  useEffect(() => {
+    setIsSaved(savedPostRecord ? true : false); // atau kamu bisa menggunakan automatic boolean assignment // setIsSaved(!!savedPostRecord) it is check whether there is a trudy value
+  }, [currentUser]);
+
+  const handleLikePost = (event: React.MouseEvent) => {
+    event.stopPropagation();
+
+    let newLikes = [...likes];
+
+    const hasLiked = newLikes.includes(userId);
+
+    if (hasLiked) {
+      newLikes = newLikes.filter((id) => id !== userId);
+    } else {
+      newLikes.push(userId);
+    }
+    setLikes(newLikes);
+    likePost({ postId: post?.$id || '', likesArray: newLikes });
+  };
+
+  const handleSavePost = (event: React.MouseEvent) => {
+    event.stopPropagation();
+
+    if (savedPostRecord) {
+      setIsSaved(false);
+      deleteSavedPost(savedPostRecord.$id);
+    } else {
+      savePost({
+        postId: post?.$id || '',
+        userId,
+      });
+      setIsSaved(true);
+    }
+  };
+
+  return (
+    <div className="flex justify-between items-center z-20">
+      <div className="flex gap-2 mr-5">
+        <img
+          src={
+            checkIsLiked(likes, userId)
+              ? "/assets/icons/liked.svg"
+              : "/assets/icons/like.svg"
+          }
+          alt="liked-icons"
+          height={20}
+          width={20}
+          onClick={handleLikePost}
+          className="cursor-pointer"
+        />
+        <p className="small-medium lg:base-medium">{likes.length}</p>
+      </div>
+
+      <div className="flex gap-2">
+        {isSavingPost || isDeletingSaved ? (
+          <Loader />
+        ) : (
+          <img
+            src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
+            alt="saved-icons"
+            height={20}
+            width={20}
+            onClick={handleSavePost}
+            className="cursor-pointer"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PostStats;
